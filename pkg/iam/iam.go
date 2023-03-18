a849e87705c6c8a4dc1a18b5b48eaada30fa01f3
MrLeea <64508229+leepala@users.noreply.github.com>
2023-03-18 13:06:09 +0800

    Revert "splite iam to single service (#26)" (#27)
    This reverts commit e734e36aeb848c2793b4f97ce44e7c88c2528036.

--- pkg/iam/iam.go
+++ pkg/iam/iam.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"errors"
 	"log"
-	"net"
-	"os"
 	"time"
 
 	iampb "github.com/leepala/OldGeneralBackend/Proto/iam"
@@ -13,29 +11,10 @@ import (
 	"github.com/leepala/OldGeneralBackend/pkg/helper"
 	"github.com/leepala/OldGeneralBackend/pkg/model"
 	uuid "github.com/satori/go.uuid"
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
 
-type server struct {
-	iampb.UnimplementedIamServer
-}
-
-func StartAndListen() {
-	var listenPort = os.Getenv("ListenPort")
-	lis, err := net.Listen("tcp", listenPort)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer()
-	iampb.RegisterIamServer(s, &server{})
-	log.Println("API Server is listening on port 30001")
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
-}
-
-func (s *server) IamLogin(ctx context.Context, in *iampb.IamLoginRequest) (*iampb.IamLoginReply, error) {
+func IamLogin(ctx context.Context, in *iampb.IamLoginRequest) (*iampb.IamLoginReply, error) {
 	log.Println("login request", in.RequestId, in.UserName)
 	var user = &model.User{}
 	err := database.GetDB().Model(&user).Where("username = ? and password = ?", in.UserName, in.Password).Find(&user).Error
@@ -57,7 +36,7 @@ func (s *server) IamLogin(ctx context.Context, in *iampb.IamLoginRequest) (*iamp
 	return reply, nil
 }
 
-func (s *server) IAMRegister(ctx context.Context, in *iampb.CreateUserRequest) (*iampb.CreateUserReply, error) {
+func IAMRegister(ctx context.Context, in *iampb.CreateUserRequest) (*iampb.CreateUserReply, error) {
 	log.Println("regist request", in.RequestId, in.UserName)
 	var counter int64 = 1
 	var user = &model.User{
@@ -91,7 +70,7 @@ func (s *server) IAMRegister(ctx context.Context, in *iampb.CreateUserRequest) (
 	return reply, nil
 }
 
-func (s *server) IAMCheckLoginStatus(ctx context.Context, in *iampb.IamCheckStatusRequest) (*iampb.IamCheckStatusReply, error) {
+func IAMCheckLoginStatus(ctx context.Context, in *iampb.IamCheckStatusRequest) (*iampb.IamCheckStatusReply, error) {
 	data, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
 		return nil, errors.New("cannot get metadata")
